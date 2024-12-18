40a0eed33813e256e956b3a905eb7b5db996fad8
ozgen <ozgn45@gmail.com>
2024-12-18 15:18:23 +0100

    add debugging examples

---
+++ debugging_ex_2.c
@@ -0,0 +1,15 @@
+//
+// Created by moezgen on 12/18/24.
+//
+#include <stdio.h>
+
+
+
+
+int main(void) {
+    int array[5] = {1, 2, 3, 4, 5};
+    int *p;
+    printf("p=%d\n: ", *p);
+
+    return 0;
+}
